dea6b11bf34c296b327a0bf5db95247077c70828
Luke <Luke.m.hanna@gmail.com>
2022-05-01 17:46:34 -0400

    Fixed database error

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
