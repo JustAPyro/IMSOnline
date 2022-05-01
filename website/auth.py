17723137e26cc05b22435ccce910e07e7d071e23
Luke <Luke.m.hanna@gmail.com>
2022-05-01 18:10:28 -0400

    Added sign in function and added extra filter to sign up options

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
 
 # Our auth blueprint defines pages
 auth = Blueprint('auth', __name__)
@@ -6,8 +9,19 @@ auth = Blueprint('auth', __name__)
 # The log-in route can accept get(load) methods and post(update) requests
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully", category='success')
+            else:
+                flash("Invalid username or password", category='error')
+        else:
+            flash('Email does not exist. Please sign up.', category='error')
+
     return render_template("sign_in.html")
 
 @auth.route('/logout')
@@ -22,7 +36,11 @@ def sign_up():
         password1 = request.form.get('password_first')
         password2 = request.form.get('password_second')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('E-mail already exists.', category='error')
+        elif len(email) < 4:
             flash('E-mail must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
@@ -31,8 +49,26 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
+            # Create a new database user
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method='sha256')
+            )
+
+            # Add the user to database
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Flash that it was successful
             flash('Account created!', category='success')
 
+            # Redirect them to the homepage
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
+
+
+
 
 
