0332293520a1f9e7162cc0b27115579739e15e31
Luke <Luke.m.hanna@gmail.com>
2022-05-04 10:26:03 -0400

    Added a DB model for inventory items

--- website/models.py
+++ website/models.py
@@ -3,6 +3,17 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    cost = db.Column(db.BigInteger)
+    link = db.Column(db.String(100))
+    source = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(10000))
@@ -16,3 +27,4 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    items = db.relationship('Item')
