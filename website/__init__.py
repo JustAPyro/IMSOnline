3d7f0e359895a6cd9800b4cdb79ac0ad748308f8
Luke <Luke.m.hanna@gmail.com>
2022-05-04 23:34:03 -0400

    fixed SQLALCHEMY_TRACK_MODIFICATIONS warning by setting it to false in config

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretkey'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     # Setup database
