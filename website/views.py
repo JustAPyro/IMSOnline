631f737dfcc8f2e37135fb9a9b904416b03b43f0
Luke <Luke.m.hanna@gmail.com>
2022-05-05 00:14:48 -0400

    added POST functionality to the add inventory page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 # Our blueprint is called views
@@ -10,8 +10,20 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/add_inventory')
+
+@views.route('/add_inventory', methods=['GET', 'POST'])
 @login_required
 def add_inventory():
-    print("Tested add inv")
+    if request.method == 'POST':
+        return _add_inventory_POST()
+    if request.method == 'GET':
+        return _add_inventory_GET()
+
+
+def _add_inventory_POST():
+    print("Posted")
+
+
+def _add_inventory_GET():
+    print("Getted")
     return render_template("add_inventory.html", user=current_user)
